cf712c2dc8fd87d7de382fbfa4cfcaed5d421a15
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-12-04 22:39:11 +0100

    fix pointer size (hopefully)

--- smart_contracts/contract/src/contract_api/circom.rs
+++ smart_contracts/contract/src/contract_api/circom.rs
@@ -1,14 +1,14 @@
 use crate::{ext_ffi, unwrap_or_revert::UnwrapOrRevert};
 use casper_types::api_error;
 use alloc::{collections::BTreeSet, vec, vec::Vec, string::String};
-
+extern crate std;
 pub fn circom_verifier(inputs: Vec<u8>) -> [u8;1]{
     // false
     let mut res: [u8;1] = [0;1];
     let result = unsafe {
         ext_ffi::casper_circom_verifier(
             inputs.as_ptr(),
-            inputs.len(),
+            inputs.len() * std::mem::size_of::<u8>(),
             res.as_mut_ptr(),
             1
         )
