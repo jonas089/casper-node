71f260cea10a2e87a4fc7bf31992bb2a079588f6
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-12-15 08:39:43 +0100

    GrothBn => GrothBls var rename

--- execution_engine/src/core/runtime/circom.rs
+++ execution_engine/src/core/runtime/circom.rs
@@ -8,7 +8,7 @@ use ark_ec::bls12::Bls12;
 use ark_bls12_377::{Bls12_377, Config};
 use serde_json;
 
-type GrothBn = Groth16<Bls12_377>;
+type GrothBls = Groth16<Bls12_377>;
 
 #[doc(hidden)]
 pub fn verify<T: AsRef<[u8]>>(
@@ -19,7 +19,7 @@ pub fn verify<T: AsRef<[u8]>>(
     let deserialized_proof: Proof<Bls12<Config>> = Proof::deserialize_uncompressed(&mut circom_proof.proof.as_slice()).unwrap();
     let deserialized_vk: PreparedVerifyingKey<Bls12<Config>> = PreparedVerifyingKey::deserialize_uncompressed(&mut circom_proof.vk.as_slice()).unwrap(); 
     // verify groth16 proof
-    if GrothBn::verify_with_processed_vk(&deserialized_vk, &deserialized_inputs, &deserialized_proof).unwrap() == true{
+    if GrothBls::verify_with_processed_vk(&deserialized_vk, &deserialized_inputs, &deserialized_proof).unwrap() == true{
         [1u8]
     }
     else{
