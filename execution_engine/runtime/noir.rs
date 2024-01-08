d8331130b7f82a704418454303fc3bdfdb9fd549
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-01-08 23:09:48 +0100

    move binary out of EE

--- execution_engine/runtime/noir.rs
+++ execution_engine/runtime/noir.rs
@@ -14,7 +14,7 @@ pub fn verify<T: AsRef<[u8]>>(
 ) -> [u8;1]{
     let noir_proof: NoirProof = serde_json::from_slice(&proof.as_ref()).unwrap(); 
     let circuit: PathBuf = PathBuf::from("/home/casper/casper-node/execution_engine/src/core/runtime/circuits/rollup");
-    let nargo: PathBuf = PathBuf::from("/home/casper/casper-node/execution_engine/src/core/runtime/binaries/nargo-linux");
+    let nargo: PathBuf = PathBuf::from("/home/casper/casper-node/binaries/nargo-linux");
     let temp_dir: tempfile::TempDir = tempdir().unwrap();
     let temp_dir: PathBuf = temp_dir.path().to_path_buf();
     let temp_src: PathBuf = temp_dir.join("src");
