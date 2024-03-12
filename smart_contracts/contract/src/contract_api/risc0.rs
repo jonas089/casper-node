83d88827bc10382ebe7cf959c5840bb80cff73ab
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-03-12 17:12:33 +0100

    remove std import

--- smart_contracts/contract/src/contract_api/risc0.rs
+++ smart_contracts/contract/src/contract_api/risc0.rs
@@ -1,7 +1,7 @@
 use crate::{ext_ffi, unwrap_or_revert::UnwrapOrRevert};
 use casper_types::api_error;
 use alloc::{collections::BTreeSet, vec, vec::Vec, string::String};
-extern crate std;
+
 pub fn risc0_verifier<T: AsRef<[u8]>>(proof: T) -> [u8;1]{
     // false
     let mut res: [u8;1] = [0;1];
