b905d1cfd43ae41cd44283767a514abaa42ba111
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-03-13 15:47:49 +0100

    uncomment the verifier

--- execution_engine/src/core/runtime/risc0_verifier.rs
+++ execution_engine/src/core/runtime/risc0_verifier.rs
@@ -7,12 +7,11 @@ use bincode;
 pub fn verify<T: AsRef<[u8]>>(
     proof_serialized: T
 ) -> [u8;1]{
-    // let risc0_proof: RiscZeroProof = bincode::deserialize(&proof_serialized.as_ref()).unwrap();
-    // let receipt: Receipt = bincode::deserialize(&risc0_proof.receipt_serialized).unwrap();
-    // let program_id: [u32;8] = risc0_proof.program_id.try_into().unwrap();
-    /*match receipt.verify(program_id){
+    let risc0_proof: RiscZeroProof = bincode::deserialize(&proof_serialized.as_ref()).unwrap();
+    let receipt: Receipt = bincode::deserialize(&risc0_proof.receipt_serialized).unwrap();
+    let program_id: [u32;8] = risc0_proof.program_id.try_into().unwrap();
+    match receipt.verify(program_id){
        Ok(_) => [1],
         Err(_) => [0]
-    }*/
-    [1u8]
+    }
 }
